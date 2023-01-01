bfeda637406b9c57fbbaf7a5f3538b86a65bdb32
irtiqayousuf <irtiqayousuf786@gmail.com>
2023-01-01 22:34:38 +0530

    use function

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import  './menu.css'
+import {Link } from "react-router-dom";
+
+function Navbar() {
+     return (
+        <>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/About">About</Link>
+              </li>
+              <li>
+                <Link to="/Contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>          
+        </>
+      )
+    };    
+export default Navbar;
