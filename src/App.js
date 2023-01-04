15f5291230fed06698ef1429978ddfc87a209606
irtiqayousuf <irtiqayousuf786@gmail.com>
2023-01-04 13:10:47 +0530

    page using funtion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import './App.css';
 import React from 'react';
 import './index.css';
-import App from './App';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navbar/>}/>
-          <Route path="Home" element={<Home/>} />
-          <Route path="About" element={<About/>} />
-          <Route path="Contact" element={<Contact/>} />          
-          
-      </Routes>
-    </BrowserRouter>
+  return(
+   <h1></h1>
   );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import Home from './pages/home';
+import About from './pages/about';
+import Explore from './pages/explore';
+import Contact from './pages/contact';
+import NotFound from './pages/notfound';
+import Menu from './pages/menu';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let menuItems = [ 
+  { id:1, name : "Home", path : "/" },
+  { id:2, name : "About", path : "/about" },
+  { id:3, name : "Contact Us", path : "/contact" },
+  { id:4, name : "Explore", path : "/explore" }
+]; 
+
+const routing = (
+  
+    <Router>
+      <Menu menuList={menuItems}/>
+        {/* first one is the default path */}
+        <Switch>
+          <Route exact path="/" component={Home}/>   
+          <Route path="/about" component={About}/>
+          <Route path="/contact" component={Contact}/>
+          <Route path="/explore" component={Explore}/>
+          <Route component={NotFound} />
+        </Switch>
+    </Router>
+  
+)
+ReactDOM.createRoot(document.getElementById('root'));
+render(routing);
