41424df0de66a81c36ba4bb949c43ee0b8679658
irtiqayousuf <irtiqayousuf786@gmail.com>
2023-01-08 13:47:49 +0530

    new

--- src/index.js
+++ src/index.js
@@ -35,3 +35,80 @@ const routing = (
 )
 ReactDOM.createRoot(document.getElementById('root'));
 render(routing);
+
+
+
+{/*ternary
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './components/Home';
+import Contact from './components/Contact';
+
+
+
+
+function Goal(props) {
+  const isGoal = props.isGoal;
+	return (
+		<>
+			{ isGoal ? <Home/> : <Contact/> }
+		</>
+	);
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Goal isGoal={false} />);
+/*}
+
+{/* logical &&
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+function Garage(props) {
+  const cars = props.cars;
+  return (
+    <>
+      <h1>Garage</h1>
+      {cars.length > 0 &&
+        <h2>
+          You have {cars.length} cars in your garage.
+        </h2>
+      }
+    </>
+  );
+}
+
+const cars = [ {name :'volvo'},
+{name :'ferrari'}
+];
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Garage cars={cars} />);
+*/}
+
+
+
+
+{           /*if else 
+  
+  
+  
+  import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './components/Home';
+import Contact from './components/Contact';
+
+function Goal(props) {
+  const isGoal = props.isGoal;
+  if (isGoal) {
+    return <Home/>;
+  }
+  return <Contact/>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+root.render(<Goal isGoal={false} />);*/}
